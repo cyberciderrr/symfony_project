cae06081073152bf1d8e60b8dc9419af466e254b
cyberciderrr <sidorenko.uliya@gmail.com>
2025-01-31 02:24:18 +1000

    added fixtures

---
+++ src/DataFixtures/ProjectGroupFixtures.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\DataFixtures;
+use App\Entity\ProjectGroup;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+class ProjectGroupFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $project = new ProjectsGroup();
+            $project->setName('Группа проекта' . $i);
+            $manager->persist($project);
+        }
+        $manager->flush();
+    }
+}
