e37225d426c004a73bb42c5b964cdae0c64742b1
cyberciderrr <sidorenko.uliya@gmail.com>
2025-02-05 15:26:05 +1000

    fixed migrations

--- migrations/Version20250205052230.php
+++ migrations/Version20250205052230.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250204052628 extends AbstractMigration
+final class Version20250205052230 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,19 +20,23 @@ final class Version20250204052628 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE project DROP CONSTRAINT FK_2FB3D0EEC31A529C');
+        $this->addSql('ALTER TABLE project ADD project_group_id UUID DEFAULT NULL');
         $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EEC31A529C FOREIGN KEY (project_group_id) REFERENCES project_group (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE task DROP CONSTRAINT FK_527EDB25166D1F9C');
+        $this->addSql('CREATE INDEX IDX_2FB3D0EEC31A529C ON project (project_group_id)');
+        $this->addSql('ALTER TABLE task ADD project_id UUID DEFAULT NULL');
         $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB25166D1F9C FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_527EDB25166D1F9C ON task (project_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE task DROP CONSTRAINT fk_527edb25166d1f9c');
-        $this->addSql('ALTER TABLE task ADD CONSTRAINT fk_527edb25166d1f9c FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE project DROP CONSTRAINT fk_2fb3d0eec31a529c');
-        $this->addSql('ALTER TABLE project ADD CONSTRAINT fk_2fb3d0eec31a529c FOREIGN KEY (project_group_id) REFERENCES project_group (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE project DROP CONSTRAINT FK_2FB3D0EEC31A529C');
+        $this->addSql('DROP INDEX IDX_2FB3D0EEC31A529C');
+        $this->addSql('ALTER TABLE project DROP project_group_id');
+        $this->addSql('ALTER TABLE task DROP CONSTRAINT FK_527EDB25166D1F9C');
+        $this->addSql('DROP INDEX IDX_527EDB25166D1F9C');
+        $this->addSql('ALTER TABLE task DROP project_id');
     }
 }
